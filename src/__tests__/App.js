d5b16a6ffe18d6f672c1de2a84baa1d56ed827af
kartavyaKloud <kartavya@kloudone.com>
2024-02-02 02:36:21 +0530

    Before , After all and Each fn use cases

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,7 +1,23 @@
 import { fireEvent, render ,screen } from "@testing-library/react"
 import App from "../App"
+import cleanDb from "../service"
 
+beforeAll(()=>{console.log("Before running all tests i will be logged")})
+beforeEach(()=>{cleanDb();})
+//similarly explore afterAll and AfterEach
 test("click event testing" , ()=> {
+render(<App/>)
+const button = screen.getByRole("button")
+fireEvent.click(button);
+expect(screen.getByText("hello")).toBeInTheDocument();
+})
+test("click event testing 2" , ()=> {
+render(<App/>)
+const button = screen.getByRole("button")
+fireEvent.click(button);
+expect(screen.getByText("hello")).toBeInTheDocument();
+})
+test("click event testing 3" , ()=> {
 render(<App/>)
 const button = screen.getByRole("button")
 fireEvent.click(button);
---
+++ src/service.js
@@ -0,0 +1,5 @@
+function cleanDb () {
+    console.log("db cleaned")
+}
+
+export default cleanDb;

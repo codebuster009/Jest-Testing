1ed0c254f065b3fb84f1996894ed41a1785317fe
kartavyaKloud <kartavya@kloudone.com>
2024-02-02 02:57:26 +0530

    Snapshot testing done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [data , setData] = useState("")
   return (
     <div className="App">
-      <h1>Test Click Event with Button {data}</h1>
+      <h1>Test Event with Button {data}</h1>
       <h1>{data}</h1>
       <button onClick={()=> {setData("hello")}}>Update</button>
       <input onChange={(e)=> {setData(e.target.value)}} type="text" placeholder="Enter Your Name Gracefully" value= {data} />
--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,25 +1,8 @@
 import { fireEvent, render ,screen } from "@testing-library/react"
 import App from "../App"
-import cleanDb from "../service"
+//Before taking the code for production if some changes occur in the component and due to that suppose the app might crash it is recommended to go with snapshot testing as it tells us clearly what recent changes have you done by taking the snapshot of the component and than if you do changes again it will say snapshot testing has failed as it didnt match the prev snapshot so if we need to update the changes simply press the "u" button and it will update the snapshot
+test("snapshot for app component" , ()=> {
+    const container = render(<App/>)
+    expect(container).toMatchSnapshot()
+})
 
-beforeAll(()=>{console.log("Before running all tests i will be logged")})
-beforeEach(()=>{cleanDb();})
-//similarly explore afterAll and AfterEach
-test("click event testing" , ()=> {
-render(<App/>)
-const button = screen.getByRole("button")
-fireEvent.click(button);
-expect(screen.getByText("hello")).toBeInTheDocument();
-})
-test("click event testing 2" , ()=> {
-render(<App/>)
-const button = screen.getByRole("button")
-fireEvent.click(button);
-expect(screen.getByText("hello")).toBeInTheDocument();
-})
-test("click event testing 3" , ()=> {
-render(<App/>)
-const button = screen.getByRole("button")
-fireEvent.click(button);
-expect(screen.getByText("hello")).toBeInTheDocument();
-})

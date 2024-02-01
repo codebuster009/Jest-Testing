6cd405509d723b02a42eedb91d26332570b5cd90
kartavyaKloud <kartavya@kloudone.com>
2024-02-01 23:25:44 +0530

    FireEvent testing of setting input to e.target.value

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [data , setData] = useState("")
   return (
     <div className="App">
-      <h1>First react test case</h1>
-      <h1>Second react test case</h1>
-      <input type="text" placeholder="Enter Your Name Gracefully" name="Kartavya" value= "Barsat singh" readOnly />
-      <img
-        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FGrand_Theft_Auto_V&psig=AOvVaw3iMufZdODAlg0R6mXBWhMQ&ust=1706872074024000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCPC9hY6AioQDFQAAAAAdAAAAABAE"
-        title="Grand Theft Auto"
-      />
+      <h1>OnChange Event with Input Text</h1>
+      <input onChange={(e)=> {setData(e.target.value + "test")}} type="text" placeholder="Enter Your Name Gracefully" value= {data} />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,71 +1,8 @@
-import {render , screen} from '@testing-library/react'
-import App from './App.js'
-test('testing app component text' , ()=> {
+import{fireEvent,render , screen} from  "@testing-library/react"
+import App from "./App"
+test("onChange event testing" , ()=> {
   render(<App/>)
-  const text = screen.getByText(/First React test case/i);
-  const text2 = screen.getByText(/sEcOnd React test case/i);
-  const title = screen.getByTitle("Grand Theft Auto")
-  expect(text).toBeInTheDocument();
-  expect(title).toBeInTheDocument();
-  expect(text2).toBeInTheDocument();
-})
-
-test("checking if input box is there" , () => {
-  render(<App/>)
-  let checkInput = screen.getByRole("textbox")
-  let checkPlaceholder = screen.getByPlaceholderText(/Enter Your Name Gracefully/i)
-  expect(checkInput).toBeInTheDocument();
-  expect(checkPlaceholder).toBeInTheDocument();
-  expect(checkInput).toHaveAttribute("name" , "Kartavya");
-  expect(checkInput).toHaveAttribute("type" , "text");
-  expect(checkInput).toHaveAttribute("value" , "Barsat singh");
-})
-describe.only("UI test cases" , ()=> {
-  test("check the input" , ()=> {
-    render(<App/>)
-    let checkInput = screen.getByRole("textbox")
-    expect(checkInput).toBeInTheDocument();
-    expect(checkInput).toHaveAttribute("type" , "text")
-  })
-  test("check the input2" , ()=> {
-    render(<App/>)
-    let checkInput = screen.getByRole("textbox")
-    expect(checkInput).toBeInTheDocument();
-    expect(checkInput).toHaveAttribute("type" , "text")
-  })
-  test("check the input3" , ()=> {
-    render(<App/>)
-    let checkInput = screen.getByRole("textbox")
-    expect(checkInput).toBeInTheDocument();
-    expect(checkInput).toHaveAttribute("type" , "text")
-  })
-})
-describe("API test cases" , ()=> {
-  test("api input" , ()=> {
-    render(<App/>)
-    let checkInput = screen.getByRole("textbox")
-    expect(checkInput).toBeInTheDocument();
-    expect(checkInput).toHaveAttribute("type" , "text")
-  })
-  test("api input2" , ()=> {
-    render(<App/>)
-    let checkInput = screen.getByRole("textbox")
-    expect(checkInput).toBeInTheDocument();
-    expect(checkInput).toHaveAttribute("type" , "text")
-  })
-  test("api input3" , ()=> {
-    render(<App/>)
-    let checkInput = screen.getByRole("textbox")
-    expect(checkInput).toBeInTheDocument();
-    expect(checkInput).toHaveAttribute("type" , "text")
-  })
-  describe('inner describe group', () => {
-    test('api nested test' , ()=> {
-      render(<App/>)
-    let checkInput = screen.getByRole("textbox")
-    expect(checkInput).toBeInTheDocument();
-    expect(checkInput).toHaveAttribute("type" , "text")
-    })
-  })
-  
+  let input = screen.getByRole("textbox")
+  fireEvent.change(input ,{target: {value: 'a'}})
+  expect(input.value).toBe("atest");
 })
